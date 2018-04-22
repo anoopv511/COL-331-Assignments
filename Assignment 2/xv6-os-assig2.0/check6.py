6f2fda8677d1cdcf21fbb4ff1e06b805def6a90e
Anoop <511anoopv@gmail.com>
2018-04-22 23:38:08 +0530

    Added more testcases and their output

---
+++ Assignment 2/xv6-os-assig2.0/check6.py
@@ -0,0 +1,15 @@
+import os
+
+os.system("make clean")
+os.system("make")
+
+print("Running 6th Testcase...")
+os.system("./test_assig2.sh assig2_6 > res_assig2_6")
+os.system("grep \"pid: 5 pr: 10\" res_assig2_6 | wc -l > testCount")
+f = open('testCount', 'r')
+result = f.read()
+result = int(result)
+if result > 0:
+	print("Pass")
+else:
+	print("Fail")
