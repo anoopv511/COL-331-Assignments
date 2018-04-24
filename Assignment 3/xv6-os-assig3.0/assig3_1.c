526c61f31cd191e16ca8001e6a7fab50975e51f4
Anoop <511anoopv@gmail.com>
2018-04-24 18:47:14 +0530

    Added testing scripts

---
+++ Assignment 3/xv6-os-assig3.0/assig3_1.c
@@ -0,0 +1,27 @@
+#include "types.h"
+#include "user.h"
+#include "uthread.c"
+
+static void 
+mythread(void)
+{
+  int i;
+  printf(1, "Thread: running\n");
+  for (i = 0; i < 5; i++) {
+    printf(1, "Thread 0x%x -> i = %d\n", (int) current_thread,i);
+    thread_yield();
+  }
+  printf(1, "Thread: exit\n");
+  current_thread->state = FREE;
+  thread_schedule();
+  return;
+}
+
+int
+main(void)
+{
+  thread_create("T0", mythread);
+  thread_create("T1", mythread);
+  thread_schedule();
+	exit();
+}
